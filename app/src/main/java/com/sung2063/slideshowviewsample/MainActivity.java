85702a8660081f55969e7cde8c63aad131f8f2b0
Sung Hyun <sunghyunb1991@gmail.com>
2021-05-01 19:52:05 -0400

    Add a Sponsor to @sung2063 GitHub Link
    Added a sponsoring link & direct link to @sung2063 GitHub profile page

--- app/src/main/java/com/sung2063/slideshowviewsample/MainActivity.java
+++ app/src/main/java/com/sung2063/slideshowviewsample/MainActivity.java
@@ -6,7 +6,11 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
+import android.graphics.Paint;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.sung2063.slideshowviewsample.adapter.MenuOptionRecyclerViewAdapter;
 import com.sung2063.slideshowviewsample.data.DataCollection;
@@ -19,6 +23,7 @@ public class MainActivity extends AppCompatActivity implements MenuOptionRecycle
     // =============================================================================================
     // Variables
     // =============================================================================================
+    private Button btSponsorship;
     private RecyclerView rvOptions;
     private MenuOptionRecyclerViewAdapter menuOptionRecyclerViewAdapter;
 
@@ -30,15 +35,23 @@ public class MainActivity extends AppCompatActivity implements MenuOptionRecycle
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        btSponsorship = findViewById(R.id.bt_sponsorship);
         rvOptions = findViewById(R.id.rv_options);
 
         // Setup Main Menu
+        btSponsorship.setPaintFlags(btSponsorship.getPaintFlags() | Paint.UNDERLINE_TEXT_FLAG);
         List<MainOptionModel> mainOptionList = DataCollection.getMainOptionData(getApplicationContext());
         rvOptions.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false));
         menuOptionRecyclerViewAdapter = new MenuOptionRecyclerViewAdapter(this, mainOptionList);
         menuOptionRecyclerViewAdapter.setListener(this);
         rvOptions.setAdapter(menuOptionRecyclerViewAdapter);
 
+        // Respond
+        btSponsorship.setOnClickListener(view -> {
+            Intent intent = new Intent(Intent.ACTION_VIEW);
+            intent.setData(Uri.parse("https://github.com/sponsors/sung2063"));
+            startActivity(intent);
+        });
     }
 
     @Override
@@ -46,21 +59,32 @@ public class MainActivity extends AppCompatActivity implements MenuOptionRecycle
 
         Intent intent = null;
         switch (position) {
-            case 0:
-                // Go to Carousel horizontal
+            case NavigateIntent.HORIZONTAL_CAROUSEL:
                 intent = new Intent(this, HorizontalCarouselSampleActivity.class);
                 break;
-            case 1:
-                // Go to Carousel vertical
+            case NavigateIntent.VERTICAL_CAROUSEL:
                 intent = new Intent(this, VerticalCarouselSampleActivity.class);
                 break;
-            case 2:
-                // Go to Slideshow
+            case NavigateIntent.SLIDESHOW:
                 intent = new Intent(this, SlideshowSampleActivity.class);
                 break;
+            case NavigateIntent.GIT_HUB_WEB_PAGE:
+                intent = new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse("https://github.com/sung2063"));
         }
 
-        if (intent != null)
+        if (intent != null) {
             startActivity(intent);
+        }
+    }
+
+    // =============================================================================================
+    // Class
+    // =============================================================================================
+    public class NavigateIntent {
+        public static final int HORIZONTAL_CAROUSEL = 0;
+        public static final int VERTICAL_CAROUSEL = 1;
+        public static final int SLIDESHOW = 2;
+        public static final int GIT_HUB_WEB_PAGE = 3;
     }
 }
